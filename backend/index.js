868de04fb891fc8ec4f2a7a179c707434a4d1133
Anay Dilip Naik <89814957+anaydilipnaik@users.noreply.github.com>
2024-05-10 02:17:14 -0700

    one more fix

--- backend/index.js
+++ backend/index.js
@@ -39,17 +39,6 @@ scpChannel.on(
 );
 scpChannel.subscribe();
 
-// Listen for changes on 'scp_availability' table
-const scpAvailabilityChannel = realtime.channel("public:scp_availability");
-scpAvailabilityChannel.on(
-  "postgres_changes",
-  { event: "UPDATE", schema: "public", table: "scp_availability" },
-  (payload) => {
-    triggerFunction(payload);
-  }
-);
-scpAvailabilityChannel.subscribe();
-
 // Function to handle the Realtime changes and perform subsequent actions
 async function triggerFunction(payload) {
   try {
@@ -103,23 +92,23 @@ async function triggerFunction(payload) {
     };
 
     // Send POST requests with the Authorization header
-    await axios.post(gupshupUrl, payloadDataPatient, config);
+    // await axios.post(gupshupUrl, payloadDataPatient, config);
 
-    const payloadDataScp = {
-      event_name: "appointment_details",
-      event_time: JSON.stringify(new Date()),
-      user: {
-        phone: data.phone,
-        name: data.name,
-        matched_person: payload.new.name,
-        meeting_date: data.date,
-        meeting_time: data.start_time + "-" + data.end_time,
-        meeting_link: "https://meet.hsciglobal.org/roundrobin/" + randomCode,
-      },
-      txid: "123",
-    };
+    // const payloadDataScp = {
+    //   event_name: "appointment_details",
+    //   event_time: JSON.stringify(new Date()),
+    //   user: {
+    //     phone: data.phone,
+    //     name: data.name,
+    //     matched_person: payload.new.name,
+    //     meeting_date: data.date,
+    //     meeting_time: data.start_time + "-" + data.end_time,
+    //     meeting_link: "https://meet.hsciglobal.org/roundrobin/" + randomCode,
+    //   },
+    //   txid: "123",
+    // };
 
-    await axios.post(gupshupUrl, payloadDataScp, config);
+    // await axios.post(gupshupUrl, payloadDataScp, config);
   } catch (err) {
     console.error("Error in trigger function:", err.message);
   }
